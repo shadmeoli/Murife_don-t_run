5a18ac45fb678c22b9dd8da2d4f1267e18f9cabf
shad <85517013+shadmeoli@users.noreply.github.com>
2022-09-30 08:39:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+# this file is esential for making python identify this as a package that can be installed and be used to generate build files
 from setuptools import setup
 from setuptools import find_packages
 
-
+# defining our CLI and the main function to call on initilization 
 setup(
 	name="murife",
 	version='0.0.1',
