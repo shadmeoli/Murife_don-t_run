ac26352f85f91d03af6cac4daf0f082e9f5373ae
shad <85517013+shadmeoli@users.noreply.github.com>
2022-09-30 08:37:15 +0300

    Update murife.py

--- murife.py
+++ murife.py
@@ -1,3 +1,9 @@
+""" 
+	When you execute this file directly it will throw an error
+	[typer.run(main) , where main -> is not defined],
+	
+	To avoid this run through the setup from the redme to get it up and running
+"""
 import time
 import emoji
 
@@ -6,7 +12,7 @@ from rich.progress import Progress, SpinnerColumn, TextColumn
 
 hes_running = emoji.emojize(":person_running:")
 
-
+# [TODO] -> This function is to print only five emojis per delay. I am to use Async await for this feature/ when I get some time today(friday/30)
 def the_runner():
 	nums = 1
 	__runner = [hes_running]
@@ -21,7 +27,7 @@ def the_runner():
 			__runner.clear()
 			__runner.append(hes_running)
 
-
+# the main entry function
 def murife():
 	while True:
 	    with Progress(
@@ -33,6 +39,6 @@ def murife():
 	        time.sleep(2)
 	    print("Murife!")
 
-
+# this will be executed while runnign the CLI from its entry point "murife"
 if __name__ == "__main__":
     typer.run(main)
